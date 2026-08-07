d64c439278de4b1ea6bb7eb9974360a5ba74c7f1
cesarzoleko <cesarzoleko@gmail.com>
2026-08-07 07:15:58 +0100

    setup EF InvoiceRepository Adapters

---
+++ Billing.Infra/Persistence/Adapters/EfInvoiceRepository.cs
@@ -0,0 +1,31 @@
+﻿using Billing.Core.Application.Ports.Out;
+using Billing.Core.Domain;
+using Billing.Infra.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Billing.Infra.Persistence.Adapters
+{
+    public class EfInvoiceRepository(BillingDbContext db) : IInvoiceRepository
+    {
+        public async Task<Invoice?> GetByIdAsync(Guid invoiceId, CancellationToken ct = default)
+        {
+            var model = await db.Invoices.Include(i => i.Payments).FirstOrDefaultAsync(i =>  i.Id == invoiceId, ct);
+
+            return model is null ? null : InvoiceModel.ToDomain(model);
+        }
+
+        public async Task SaveAsync(Invoice invoice, CancellationToken ct = default)
+        {
+            var model = InvoiceModel.ToPersistence(invoice.ToSnapshot());
+            await db.Invoices.AddAsync(model, ct);
+            await db.SaveChangesAsync(ct);
+        }
+
+        public async Task UpdateAsync(Invoice invoice, CancellationToken ct = default)
+        {
+            var model = InvoiceModel.ToPersistence(invoice.ToSnapshot());
+            db.Invoices.Update(model);
+            await db.SaveChangesAsync(ct);
+        }
+    }
+}
